764f4a3ad1c491183bc6a21b8c9a66c3cf40a887
duncsully <duncsully@gmail.com>
2025-09-16 21:27:32 -0700

    feat: problem 5 solver

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
 fn main() {
-    println!("{}", solver4());
+    println!("{}", solver5());
 }
 
 /// Finds the sum of all multiples of 3 and 5 by finding the sums of each individually and then
@@ -159,6 +159,15 @@ fn solver4() -> u32 {
             }
         })
         .unwrap()
+}
+
+/// They gave us an easy one this time as far as finding the mathgic. Just need to use
+/// prime factorization and use the highest exponent for each prime that occurs in any
+/// of the integers from 1-20. This could technically be generalized using my PrimeNumbers
+/// struct to get prime factors for any arbitrary range, calculating and storing the max
+/// exponent, but...maybe another time.
+fn solver5() -> u32 {
+    2u32.pow(4) * 3u32.pow(2) * 5 * 7 * 11 * 13 * 17 * 19
 }
 
 #[cfg(test)]
