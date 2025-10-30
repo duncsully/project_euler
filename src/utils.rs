dc9b7b95306ac3b8c4eab1b990e12bf829aef09f
duncsully <duncsully@gmail.com>
2025-10-29 17:37:13 -0700

    feat: gcd util

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
+use std::cmp::min;
 use std::collections::VecDeque;
+use std::mem::swap;
 use std::ops::{AddAssign, MulAssign};
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Hash)]
@@ -270,6 +272,49 @@ impl Iterator for Primes {
 
         self.cache.push(next_prime);
         Some(next_prime)
+    }
+}
+
+// From Wikipedia (is that better than AI? Either way, yes, the laziness is setting in)
+pub fn gcd(mut u: u64, mut v: u64) -> u64 {
+    // Base cases: gcd(n, 0) = gcd(0, n) = n
+    if u == 0 {
+        return v;
+    } else if v == 0 {
+        return u;
+    }
+
+    // Using identities 2 and 3:
+    // gcd(2ⁱ u, 2ʲ v) = 2ᵏ gcd(u, v) with u, v odd and k = min(i, j)
+    // 2ᵏ is the greatest power of two that divides both 2ⁱ u and 2ʲ v
+    let i = u.trailing_zeros();
+    u >>= i;
+    let j = v.trailing_zeros();
+    v >>= j;
+    let k = min(i, j);
+
+    loop {
+        // u and v are odd at the start of the loop
+        debug_assert!(u % 2 == 1, "u = {} should be odd", u);
+        debug_assert!(v % 2 == 1, "v = {} should be odd", v);
+
+        // Swap if necessary so u ≤ v
+        if u > v {
+            swap(&mut u, &mut v);
+        }
+
+        // Identity 4: gcd(u, v) = gcd(u, v-u) as u ≤ v and u, v are both odd
+        v -= u;
+        // v is now even
+
+        if v == 0 {
+            // Identity 1: gcd(u, 0) = u
+            // The shift by k is necessary to add back the 2ᵏ factor that was removed before the loop
+            return u << k;
+        }
+
+        // Identity 3: gcd(u, 2ʲ v) = gcd(u, v) as u is odd
+        v >>= v.trailing_zeros();
     }
 }
 
